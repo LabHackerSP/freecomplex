0adcc8d6d4d1a7c721da9f8781b3192ef5fc8e00
Pedro Markun <pedro@markun.com.br>
2015-06-09 00:55:35 -0300

    funcao de password

--- setup.sh
+++ setup.sh
@@ -1,10 +1,35 @@
 clear
 PS1="\W> "
-bind 'set disable-completion on'
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#bind 'set disable-completion on'
 cd() {
   builtin cd "$@"
-  clear
-  if [ -f ".env" ]; then
-	source .env
+  if [ -f ".secret" ]; then
+     secret=($(cat .secret))
+     password $secret
   fi
+  if [ -f ".env" ]; then
+	   source .env
+  fi
+}
+
+password() {
+  echo -n "Password: "
+  read password
+  hash=($(echo -n "$password" | md5sum))
+  if [ "$hash" == "$@" ]; then
+    clear
+  else
+   echo "Password incorreto!"
+   cd ..
+  fi
+}
+
+create_secret() {
+  $(echo -n "$@" | md5sum > .secret) 
+  echo "Password: " $@
+}
+reload() {
+  source $DIR/setup.sh
 }
